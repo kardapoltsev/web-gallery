d767cda5fe81bf1fd6272074e100d37f9f1e536c
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2014-08-19 08:51:04 +0600

    Likes DAO impl

---
+++ src/main/scala/com/github/kardapoltsev/webgallery/db/gen/Like.scala
@@ -0,0 +1,102 @@
+package com.github.kardapoltsev.webgallery.db.gen
+
+import scalikejdbc._
+import org.joda.time.{DateTime}
+
+case class Like(
+  id: Int, 
+  imageId: Int, 
+  ownerId: Int, 
+  createTime: DateTime) {
+
+  def save()(implicit session: DBSession = Like.autoSession): Like = Like.save(this)(session)
+
+  def destroy()(implicit session: DBSession = Like.autoSession): Unit = Like.destroy(this)(session)
+
+}
+      
+
+object Like extends SQLSyntaxSupport[Like] {
+
+  override val tableName = "likes"
+
+  override val columns = Seq("id", "image_id", "owner_id", "create_time")
+
+  def apply(l: SyntaxProvider[Like])(rs: WrappedResultSet): Like = apply(l.resultName)(rs)
+  def apply(l: ResultName[Like])(rs: WrappedResultSet): Like = new Like(
+    id = rs.get(l.id),
+    imageId = rs.get(l.imageId),
+    ownerId = rs.get(l.ownerId),
+    createTime = rs.get(l.createTime)
+  )
+      
+  val l = Like.syntax("l")
+
+  override val autoSession = AutoSession
+
+  def find(id: Int)(implicit session: DBSession = autoSession): Option[Like] = {
+    withSQL {
+      select.from(Like as l).where.eq(l.id, id)
+    }.map(Like(l.resultName)).single.apply()
+  }
+          
+  def findAll()(implicit session: DBSession = autoSession): List[Like] = {
+    withSQL(select.from(Like as l)).map(Like(l.resultName)).list.apply()
+  }
+          
+  def countAll()(implicit session: DBSession = autoSession): Long = {
+    withSQL(select(sqls"count(1)").from(Like as l)).map(rs => rs.long(1)).single.apply().get
+  }
+          
+  def findAllBy(where: SQLSyntax)(implicit session: DBSession = autoSession): List[Like] = {
+    withSQL { 
+      select.from(Like as l).where.append(sqls"${where}")
+    }.map(Like(l.resultName)).list.apply()
+  }
+      
+  def countBy(where: SQLSyntax)(implicit session: DBSession = autoSession): Long = {
+    withSQL { 
+      select(sqls"count(1)").from(Like as l).where.append(sqls"${where}")
+    }.map(_.long(1)).single.apply().get
+  }
+      
+  def create(
+    imageId: Int,
+    ownerId: Int,
+    createTime: DateTime)(implicit session: DBSession = autoSession): Like = {
+    val generatedKey = withSQL {
+      insert.into(Like).columns(
+        column.imageId,
+        column.ownerId,
+        column.createTime
+      ).values(
+        imageId,
+        ownerId,
+        createTime
+      )
+    }.updateAndReturnGeneratedKey.apply()
+
+    Like(
+      id = generatedKey.toInt, 
+      imageId = imageId,
+      ownerId = ownerId,
+      createTime = createTime)
+  }
+
+  def save(entity: Like)(implicit session: DBSession = autoSession): Like = {
+    withSQL {
+      update(Like).set(
+        column.id -> entity.id,
+        column.imageId -> entity.imageId,
+        column.ownerId -> entity.ownerId,
+        column.createTime -> entity.createTime
+      ).where.eq(column.id, entity.id)
+    }.update.apply()
+    entity
+  }
+        
+  def destroy(entity: Like)(implicit session: DBSession = autoSession): Unit = {
+    withSQL { delete.from(Like).where.eq(column.id, entity.id) }.update.apply()
+  }
+        
+}
---
+++ src/test/scala/com/github/kardapoltsev/webgallery/db/gen/LikeSpec.scala
@@ -0,0 +1,62 @@
+package com.github.kardapoltsev.webgallery.db.gen
+
+
+import com.github.kardapoltsev.webgallery.Database
+import org.scalatest._
+import org.joda.time._
+import scalikejdbc.scalatest.AutoRollback
+import scalikejdbc._
+
+class LikeSpec extends fixture.FlatSpec with Matchers with AutoRollback with FakeDataCreator {
+  val l = Like.syntax("l")
+
+  Database
+  behavior of "Like"
+
+  it should "find by primary keys" in { implicit session =>
+    createLike()
+    val maybeFound = Like.find(123)
+    maybeFound.isDefined should be(true)
+  }
+  it should "find all records" in { implicit session =>
+    createLike()
+    val allResults = Like.findAll()
+    allResults.size should be >(0)
+  }
+  it should "count all records" in { implicit session =>
+    createLike()
+    val count = Like.countAll()
+    count should be >(0L)
+  }
+  it should "find by where clauses" in { implicit session =>
+    createLike()
+    val results = Like.findAllBy(sqls.eq(l.id, 123))
+    results.size should be >(0)
+  }
+  it should "count by where clauses" in { implicit session =>
+    createLike()
+    val count = Like.countBy(sqls.eq(l.id, 123))
+    count should be >(0L)
+  }
+  it should "create new record" in { implicit session =>
+    createImage()
+    val created = Like.create(imageId = 123, ownerId = 123, createTime = DateTime.now)
+    created should not be(null)
+  }
+  it should "save a record" in { implicit session =>
+    createLike()
+    val entity = Like.findAll().head
+    val modified = entity
+    val updated = Like.save(modified.copy(createTime = DateTime.now()))
+    updated should not equal(entity)
+  }
+  it should "destroy a record" in { implicit session =>
+    createLike()
+    val entity = Like.findAll().head
+    Like.destroy(entity)
+    val shouldBeNone = Like.find(123)
+    shouldBeNone.isDefined should be(false)
+  }
+
+}
+        
