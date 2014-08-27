59f941f1dd08d48510015f156570cb490bb0259b
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2014-08-27 11:41:35 +0600

    get current user request test

--- src/test/scala/com/github/kardapoltsev/webgallery/http/UserSprayServiceSpec.scala
+++ src/test/scala/com/github/kardapoltsev/webgallery/http/UserSprayServiceSpec.scala
@@ -1,6 +1,8 @@
 package com.github.kardapoltsev.webgallery.http
 
 
+import com.github.kardapoltsev.webgallery.db.gen.User
+import org.joda.time.{DateTimeZone, DateTime}
 import spray.routing.HttpService
 import spray.testkit.ScalatestRouteTest
 import org.scalatest.{FlatSpec, Matchers}
@@ -20,14 +22,21 @@ class UserSprayServiceSpec extends FlatSpec with Matchers with ScalatestRouteTes
   with HttpService with UserSprayService {
 
   import BaseSprayService._
+  import marshalling._
 
   override def actorRefFactory = system
   override implicit val executionContext = system.dispatcher
   override implicit val requestTimeout = Timeout(FiniteDuration(3, concurrent.duration.SECONDS))
+
   override protected def registerUser(r: RegisterUser): Result[AuthResponse] =
     Future.successful(Left(ErrorResponse.Conflict))
+
   override protected def getUser(r: GetUser): Result[GetUserResponse] =
     Future.successful(Left(ErrorResponse.NotFound))
+
+  val getCurrentUserResponse = GetUserResponse(User(0, "currentUser", "avatarId", DateTime.now(DateTimeZone.UTC)))
+  override protected def getCurrentUser(r: GetCurrentUser.type): Result[GetUserResponse] =
+    Future.successful(Right(getCurrentUserResponse))
 
   behavior of "UsersSprayService"
 
@@ -42,6 +51,13 @@ class UserSprayServiceSpec extends FlatSpec with Matchers with ScalatestRouteTes
   it should "handle get user request" in {
     Get("/api/users/123") ~> usersRoute ~> check {
       status should be(StatusCodes.NotFound)
+    }
+  }
+
+  it should "handle get current user request" in {
+    Get("/api/users") ~> usersRoute ~> check {
+      status should be(StatusCodes.OK)
+      responseAs[GetUserResponse] should be(getCurrentUserResponse)
     }
   }
 
