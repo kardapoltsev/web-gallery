6b329ce64531a84c2b748892a6cecd53c8b3e871
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2014-09-18 13:50:10 +0600

    adding /etc/webgallery configs to package

--- src/main/scala/com/github/kardapoltsev/webgallery/Configs.scala
+++ src/main/scala/com/github/kardapoltsev/webgallery/Configs.scala
@@ -4,11 +4,16 @@ package com.github.kardapoltsev.webgallery
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 
+import scala.reflect.io.File
+
+
+
 /**
  * Created by alexey on 5/26/14.
  */
 object Configs {
-  private val config = ConfigFactory.load()
+  private val defaultConfig = ConfigFactory.load()
+  private val config = ConfigFactory.parseFile(File("conf/application.conf").jfile) .withFallback(defaultConfig)
   val OriginalsDir = config.getString("server.images.originals.dir")
   val AlternativesDir = config.getString("server.images.alternatives.dir")
   val UnprocessedDir = config.getString("server.images.unprocessed.dir")
