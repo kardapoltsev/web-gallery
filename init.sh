2afe5a67f987c438eb27045903a74d8930675e74
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2014-09-18 13:20:46 +0600

    added configs for production

--- init.sh
+++ init.sh
@@ -1,7 +1,2 @@
-echo "NOTE: should be run as root, first parameter should be user that will run webgallery"
-user=root
-if [ ! -z $1 ]; then
-  user=$1
-fi
-install -m 777 -d /var/lib/webgallery
+echo "NOTE: should be run as root"
 su postgres -c "psql -f initdb.sql"
