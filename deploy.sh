10675893cf58abb78c27f3672ab23c8603e2fd39
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2014-10-05 22:17:28 +0600

    web build added to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+echo "building web"
+cd web
+r.js -o app.build.js
+cd ..
+
+echo "building server"
 sbt 'debian:packageBin'
 filename=webgallery_1.0.0-SNAPSHOT_amd64.deb
 scp ./target/$filename sunrise:
