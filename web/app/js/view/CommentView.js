7e60594e6afb24072352a359d61fbea586f906e0
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2015-01-14 15:44:14 +0500

    UI for deleting comments

--- web/app/js/view/CommentView.js
+++ web/app/js/view/CommentView.js
@@ -8,7 +8,12 @@ define(function(require){
       ;
 
   return Backbone.View.extend({
-    el: function() {
+    tagName: "div",
+    className: "panel-group",
+    id: function() {
+      return "#comment-" + this.model.id;
+    },
+    parentEl: function() {
       var parent = this.model.get("parentCommentId");
       console.log("parent comment id is " + parent);
       if(parent == this.model.id){
@@ -23,19 +28,31 @@ define(function(require){
     initialize: function() {
       console.log("init comment view");
       this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
       this.render();
     },
 
 
     render: function() {
       console.log("render comment view");
-      this.$el.append(this.template(this.model.toJSON()));
+      this.$el.html(this.template(this.model.toJSON()));
+      $(this.parentEl()).append(this.$el);
       $("#reply-button-" + this.model.id).click(this.onReplyClick.bind(this));
+
+      console.log(this.$el);
       //TODO: hide by default, show for owner
       if(this.model.get("ownerId") != window.galleryUser.id) {
         $("#delete-comment-" + this.model.id).hide();
+      } else {
+        $("#delete-comment-" + this.model.id).click(this.onDeleteClick.bind(this))
       }
       return this;
+    },
+
+
+    onDeleteClick: function() {
+      console.log("deleting comment " + this.model.id);
+      this.model.destroy();
     },
 
 
