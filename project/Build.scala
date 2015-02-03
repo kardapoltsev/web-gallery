3cf1f531955b904b997099225f57b906ce579797
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2015-02-03 12:21:41 +0500

    dependency updates

--- project/Build.scala
+++ project/Build.scala
@@ -125,13 +125,13 @@ object ApplicationBuild extends Build {
 }
 
 object Versions {
-  val ScalikejdbcVersion = "2.2.2"
+  val ScalikejdbcVersion = "2.2.3"
   val CommonsIoVersion = "2.4"
-  val MetadataExtractorVersion = "2.7.0"
+  val MetadataExtractorVersion = "2.7.2"
   val LogbackVersion = "1.1.2"
   val scalaVer = "2.11.5"
   val AkkaVersion = "2.3.9"
   val SprayJson = "1.3.1"
   val SprayVersion = "1.3.2"
-  val ScalaTestVersion = "2.2.3"
+  val ScalaTestVersion = "2.2.4"
 }
